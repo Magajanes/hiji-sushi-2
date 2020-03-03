9616c08a92791212b0b173eeabaa3f45933e9f36
Magajanes <thiagosmfis@gmail.com>
2020-03-02 22:37:07 -0300

    Create ingredient drag logic

---
+++ Assets/Scripts/Ingredient.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ingredients
+{
+    public class Ingredient : MonoBehaviour
+    {
+        [SerializeField]
+        private IngredientName Name;
+
+        [SerializeField]
+        private GameObject piecePrefab;
+
+        [SerializeField]
+        private new Collider2D collider;
+
+        private GameObject piece = null;
+        private Collider2D pieceCollider;
+        private Vector3 position;
+
+        public void PickUp()
+        {
+            if (piece == null)
+            {
+                piece = Instantiate(piecePrefab, transform.position, Quaternion.identity);
+                pieceCollider = piece.GetComponent<Collider2D>();
+            }
+        }
+
+        public void Move()
+        {
+            position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            position.z = 0;
+            piece.transform.position = position;
+        }
+
+        public void Release(Collider2D cuttingBoard)
+        {
+            if (pieceCollider.IsTouching(cuttingBoard))
+            {
+                Debug.Log(Name + " used on cutting board!");
+            }
+            else
+            {
+                Destroy(piece.gameObject);
+            }
+
+            piece = null;
+        }
+    }
+}
---
+++ Assets/Scripts/IngredientName.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ingredients
+{
+    public enum IngredientName
+    {
+        Rice = 0,
+        Salmon = 1,
+        Tuna = 2,
+        Wasabi = 3
+    }
+}
